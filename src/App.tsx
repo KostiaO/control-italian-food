6b1107aa553830a5c2b633f89aac6982211ef562
Kostya Omelchenko <kostiaomelcenko@gmail.com>
2023-06-06 14:45:27 +0200

    project initialized + tailwind configs

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Main } from './View/Pages/Main'
+import { Header } from './View/Sections/Header'
+
+function App() {
+  return (
+    <>
+      <div className="bg-white">
+        <Header />
+        <Main />        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/View/Pages/Main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { MainLayout } from '../Layouts/MainLayout';
+
+export const Main: React.FC = () => {
+    return (
+        <MainLayout />
+    );
+}
